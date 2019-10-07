ebddfea7ad1db975dd580afbf375e36814dd65e8
xoanyiwo <onyemobi.anyiwo@jmsmucker.com>
2019-10-07 16:16:48 -0400

    Heroku changes 2

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(function(req, res, next){
 	} else {
 		res.redirect('https://' + req.hostname + req.url)
 	}
-})
+});
  
 app.use(express.static("public"));
 
 app.listen(port, function(){
 	console.log("Express server is up on port " + PORT);
-})
+});
