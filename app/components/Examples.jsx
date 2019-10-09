cb3635a3c83119734664f04faf9e2eb00360595c
xoanyiwo <onyemobi.anyiwo@jmsmucker.com>
2019-10-09 15:56:56 -0400

    New styling 2

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Example = (props) => 
 {
-	return(
-			<h3>Example Component</h3>
+	return(	<div>
+				<h1 className="text-center">Example Component</h1>
+					<p>Here are a few example locations to try out:</p>
+					<ol>
+						<li>
+							<Link to='/?location=Houston'>Houston, Texas</Link>
+						</li>
+						<li>
+							<Link to='/?location=Dallas'>Dallas, Texas</Link>
+						</li>
+					</ol>
+			</div>
 		);
 }
 
